b20fa7a94c4a2301c8d98e9ab90056b98bffce44
NNR <nillanageswararao075@gmail.com>
2023-06-30 08:07:07 +0000

    add src folder

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,13 @@
+// Function Component
+// const Welcome = () => <h1>Hello, User</h1>
+
+import {Component} from 'react'
+// Class Component
+class Welcome extends Component {
+  render() {
+    const {name} = this.props
+    return <h1>Hello, {name}!</h1>
+  }
+}
+
+export default Welcome
